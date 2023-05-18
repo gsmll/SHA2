83d825c4eeee6a2c286ce5f5b61c73ae18544913
Jeffrey Jiang <jjiang30@stuy.edu>
2023-05-17 20:28:16 -0400

    added more stub to sha256

--- include/hash/SHA256.hpp
+++ include/hash/SHA256.hpp
@@ -4,15 +4,21 @@
 #include "Utility.hpp"
 #include "Hash.hpp"
 
-Hash<256> general_sha256(const char* input);
+namespace _details
+{
+    std::uint8_t* preprocess_sha256(const char* input, std::size_t blks )
+
+    Hash<256> general_sha256(const char* input);
 
 #ifdef __AVX2__
-Hash<256> simd_sha256(const char* input);
+    Hash<256> simd_sha256(const char* input);
 #endif
 
 #ifdef __SHA__
-Hash<256> instruction_sha256(const char* input);
+    Hash<256> instruction_sha256(const char* input);
 #endif
+
+}
 
 Hash<256> sha256(const char* input);
 

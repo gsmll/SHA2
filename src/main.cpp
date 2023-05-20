3ad32c6ec6c40df3d346645d653aadd07e138630
Jeffrey Jiang <jjiang30@stuy.edu>
2023-05-20 01:02:31 -0400

    added user_defined literal for hash obj + TEST macro for testing + started work on SHA224

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <bitset>
+#include <cassert>
 
 #include "Hash.hpp"
 #include "hash/SHA256.hpp"
 
+#define TEST(expr, msg)          \
+if (expr) std::cout << "PASSED TEST [" << msg << "]\n"; \
+else std::cout << "FAILED TEST [" << msg << "]\n"; 
+
 int main()
 {
-    // std::cout << _details::general_sha256("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq") << "\n";
+    TEST(sha256("abc") == 0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad_hash, "SHA256 SHORT TEST");
+    TEST(sha256("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq") == 0x248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1_hash, "SHA256 MEDIUM TEST");
 
     char* buffer = new char[1000001];
     buffer[1000000] = '\0';
@@ -13,8 +19,6 @@ int main()
     {
         buffer[i] = 'a';
     }
-
-    std::cout << sha256(buffer) << "\n";
-
+    TEST(sha256(buffer) == 0xcdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0_hash, "SHA256 LONG LONG MESSAGE TEST");
     delete[] buffer;
 }

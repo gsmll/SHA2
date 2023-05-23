4aa0a3fa41efc3778f687ba446eb3aae1d47e5f5
Jeffrey Jiang <jjiang30@stuy.edu>
2023-05-23 04:44:19 -0400

    began working on SHA instruction implementation

--- src/SHA256.cpp
+++ src/SHA256.cpp
@@ -295,6 +295,51 @@ namespace _details
     }
 #endif
 
+#ifdef __SHA__
+
+    [[nodiscard]] Hash<256> instruction_sha256(const char* input)
+    {
+        using word = std::uint32_t;
+
+        constexpr std::size_t bits_per_block = 512;
+        constexpr std::size_t bytes_per_block = bits_per_block / 8;
+        constexpr std::size_t rounds_per_chunk = 64;
+        constexpr word round_values[64]{ 
+            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+            0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+            0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+            0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+            0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+        };
+
+        std::size_t blk_count;
+        std::uint8_t* blks = preprocess_sha256(input, &blk_count);
+
+        __m128i hash_CDGH = _mm_set_epi32(0x3c6ef372, 0xa54ff53a, 0x1f83d9ab, 0x5be0cd19);
+        __m128i hash_ABEF = _mm_set_epi32(0x6a09e667, 0xbb67ae85, 0x510e527f, 0x9b05688c);
+
+        std::uint8_t* ptr = blks;
+        for (std::size_t chunk_idx = 0; chunk_idx < blk_count; ++chunk_idx, ptr += bytes_per_block)
+        {
+            __m128i CDGF = hash_CDGH;
+            __m128i ABEF = hash_ABEF;
+            __m128i temp_ABEF;
+
+            __m128i msg = _mm_loadu_si128((__m128i_u*) ptr);
+            // flip msg bytes
+            __m128i M = _mm_add_epi32(msg, _mm_set_epi32(0xe9b5dba5, 0xb5c0fbcf, 0x71374491, 0x428a2f98));
+
+        }
+
+        delete[] blks;
+        return {};
+    }
+
+#endif
+
 }
 
 [[nodiscard]] Hash<256> sha256(const char* input)

4b7938bc2ed7a2f528d3089e9db511e370a81a06
Jeffrey Jiang <jjiang30@stuy.edu>
2023-05-25 15:10:51 -0400

    completed main loop for SHA intrinsic code

--- src/SHA256.cpp
+++ src/SHA256.cpp
@@ -322,15 +322,6 @@ namespace _details
         constexpr std::size_t bits_per_block = 512;
         constexpr std::size_t bytes_per_block = bits_per_block / 8;
         constexpr std::size_t rounds_per_chunk = 64;
-        constexpr word round_values[]{ 
-            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-            0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-            0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-            0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-        };
-        word hash_data[8]{ 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
 
         std::size_t blk_count;
         std::uint8_t* blks = preprocess_sha256(input, &blk_count);
@@ -340,7 +331,8 @@ namespace _details
 
         std::uint8_t* ptr = blks;
         __m128i CDGH, ABEF;
-        __m128i msg, _msg0, _msg1, sig0_16, sig0_20, sig0_24, sig0_28;
+        __m128i msg, _msg0, _msg1, _msg2, _msg3; // Every 16 words, with 4 rounds each, requires 4 _msg variables
+        __m128i t_0; // for W[t-7] in adding values in message schedule queue
         __m128i big_endian_flip_mask = _mm_set_epi8(12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3);
 
 #ifdef DEBUG
@@ -352,9 +344,9 @@ namespace _details
             CDGH = hash_CDGH;
             ABEF = hash_ABEF;
 
-            // round 1-3
-            msg = _mm_loadu_si128((__m128i_u*) ptr);
-            _msg0 = _mm_shuffle_epi8(msg, big_endian_flip_mask); // flip to W19, W18, W17, W16
+            // round 0-3
+            _msg0 = _mm_loadu_si128((__m128i_u*) ptr);
+            _msg0 = _mm_shuffle_epi8(_msg0, big_endian_flip_mask); // flip to W19, W18, W17, W16
             msg = _mm_add_epi32(_msg0, _mm_set_epi32(0xe9b5dba5, 0xb5c0fbcf, 0x71374491, 0x428a2f98)); // add to M19, M18, M17, M16
             CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
             DEBUG_ABCDEFGH(CDGH, ABEF);
@@ -363,8 +355,8 @@ namespace _details
             DEBUG_ABCDEFGH(ABEF, CDGH);
 
             // round 4-7
-            msg = _mm_loadu_si128((__m128i_u*) (ptr + 1 * sizeof(word) * 4));
-            _msg1 = _mm_shuffle_epi8(msg, big_endian_flip_mask); 
+            _msg1 = _mm_loadu_si128((__m128i_u*) (ptr + 1 * sizeof(word) * 4));
+            _msg1 = _mm_shuffle_epi8(_msg1, big_endian_flip_mask); 
             msg = _mm_add_epi32(_msg1, _mm_set_epi32(0xab1c5ed5, 0x923f82a4, 0x59f111f1, 0x3956c25b));
             CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
             DEBUG_ABCDEFGH(CDGH, ABEF);
@@ -372,36 +364,210 @@ namespace _details
             ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
             DEBUG_ABCDEFGH(ABEF, CDGH);
 
-            sig0_16 = _mm_sha256msg1_epu32(_msg0, _msg1); // SIGMA0 for RUN 16
-
             // round 8 - 11
-            msg = _mm_loadu_si128((__m128i_u*) (ptr + 2 * sizeof(word) * 4));
-            _msg0 = _mm_shuffle_epi8(msg, big_endian_flip_mask); 
-            msg = _mm_add_epi32(_msg0, _mm_set_epi32(0x550c7dc3, 0x243185be, 0x12835b01, 0xd807aa98));
+            _msg2 = _mm_loadu_si128((__m128i_u*) (ptr + 2 * sizeof(word) * 4));
+            _msg2 = _mm_shuffle_epi8(_msg2, big_endian_flip_mask); 
+            msg = _mm_add_epi32(_msg2, _mm_set_epi32(0x550c7dc3, 0x243185be, 0x12835b01, 0xd807aa98));
             CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
             DEBUG_ABCDEFGH(CDGH, ABEF);
             msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
             ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
             DEBUG_ABCDEFGH(ABEF, CDGH);
 
-            sig0_20 = _mm_sha256msg1_epu32(_msg1, _msg0); // SIGMA0 for RUN 20
+            _msg0 = _mm_sha256msg1_epu32(_msg0, _msg1); // SIGMA0 for RUN 16
 
             // round 12 - 15
-            msg = _mm_loadu_si128((__m128i_u*) (ptr + 3 * sizeof(word) * 4));
-            _msg1 = _mm_shuffle_epi8(msg, big_endian_flip_mask); 
-            msg = _mm_add_epi32(_msg1, _mm_set_epi32(0xc19bf174, 0x9bdc06a7, 0x80deb1fe, 0x72be5d74));
+            _msg3 = _mm_loadu_si128((__m128i_u*) (ptr + 3 * sizeof(word) * 4));
+            _msg3 = _mm_shuffle_epi8(_msg3, big_endian_flip_mask); 
+            msg = _mm_add_epi32(_msg3, _mm_set_epi32(0xc19bf174, 0x9bdc06a7, 0x80deb1fe, 0x72be5d74));
             CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
             DEBUG_ABCDEFGH(CDGH, ABEF);
             msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
             ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
             DEBUG_ABCDEFGH(ABEF, CDGH);
 
-            sig0_24 = _mm_sha256msg1_epu32(_msg0, _msg1); // SIGMA0 for RUN 24
+            t_0 = _mm_alignr_epi8(_msg3, _msg2, 4); // W[t-7] for RUN 16
+            _msg0 = _mm_add_epi32(_msg0, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg1 = _mm_sha256msg1_epu32(_msg1, _msg2); // SIGMA0 for RUN 20
 
             // begin calculating messages
-            // round 16 - 19
 
+            // round 16 - 19
+            _msg0 = _mm_sha256msg2_epu32(_msg0, _msg3);
+            msg = _mm_add_epi32(_msg0, _mm_set_epi32(0x240ca1cc, 0x0fc19dc6, 0xefbe4786, 0xe49b69c1));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg0, _msg3, 4); // W[t-7] for RUN 20
+            _msg1 = _mm_add_epi32(_msg1, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg2 = _mm_sha256msg1_epu32(_msg2, _msg3); // SIGMA0 for RUN 24
+
+            // round 20 - 23
+            _msg1 = _mm_sha256msg2_epu32(_msg1, _msg0);
+            msg = _mm_add_epi32(_msg1, _mm_set_epi32(0x76f988da, 0x5cb0a9dc, 0x4a7484aa, 0x2de92c6f));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg1, _msg0, 4); // W[t-7] for RUN 24
+            _msg2 = _mm_add_epi32(_msg2, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg3 = _mm_sha256msg1_epu32(_msg3, _msg0); // SIGMA0 for RUN 28
+
+            // round 24 - 27
+            _msg2 = _mm_sha256msg2_epu32(_msg2, _msg1);
+            msg = _mm_add_epi32(_msg2, _mm_set_epi32(0xbf597fc7, 0xb00327c8, 0xa831c66d, 0x983e5152));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg2, _msg1, 4); // W[t-7] for RUN 28
+            _msg3 = _mm_add_epi32(_msg3, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg0 = _mm_sha256msg1_epu32(_msg0, _msg1); // SIGMA0 for RUN 32
+
+            // round 28 - 31
+            _msg3 = _mm_sha256msg2_epu32(_msg3, _msg2);
+            msg = _mm_add_epi32(_msg3, _mm_set_epi32(0x14292967, 0x06ca6351, 0xd5a79147, 0xc6e00bf3));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg3, _msg2, 4); // W[t-7] for RUN 32
+            _msg0 = _mm_add_epi32(_msg0, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg1 = _mm_sha256msg1_epu32(_msg1, _msg2); // SIGMA0 for RUN 36
+
+            // round 32 - 35
+            _msg0 = _mm_sha256msg2_epu32(_msg0, _msg3);
+            msg = _mm_add_epi32(_msg0, _mm_set_epi32(0x53380d13, 0x4d2c6dfc, 0x2e1b2138, 0x27b70a85));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg0, _msg3, 4); // W[t-7] for RUN 36
+            _msg1 = _mm_add_epi32(_msg1, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg2 = _mm_sha256msg1_epu32(_msg2, _msg3); // SIGMA0 for RUN 40
+
+            // round 36 - 39
+            _msg1 = _mm_sha256msg2_epu32(_msg1, _msg0);
+            msg = _mm_add_epi32(_msg1, _mm_set_epi32(0x92722c85, 0x81c2c92e, 0x766a0abb, 0x650a7354));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg1, _msg0, 4); // W[t-7] for RUN 40
+            _msg2 = _mm_add_epi32(_msg2, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg3 = _mm_sha256msg1_epu32(_msg3, _msg0); // SIGMA0 for RUN 44
+
+            // round 40 - 43
+            _msg2 = _mm_sha256msg2_epu32(_msg2, _msg1);
+            msg = _mm_add_epi32(_msg2, _mm_set_epi32(0xc76c51a3, 0xc24b8b70, 0xa81a664b, 0xa2bfe8a1));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg2, _msg1, 4); // W[t-7] for RUN 44
+            _msg3 = _mm_add_epi32(_msg3, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg0 = _mm_sha256msg1_epu32(_msg0, _msg1); // SIGMA0 for RUN 48
+
+            // round 44 - 47
+            _msg3 = _mm_sha256msg2_epu32(_msg3, _msg2);
+            msg = _mm_add_epi32(_msg3, _mm_set_epi32(0x106aa070, 0xf40e3585, 0xd6990624, 0xd192e819));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg3, _msg2, 4); // W[t-7] for RUN 48
+            _msg0 = _mm_add_epi32(_msg0, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg1 = _mm_sha256msg1_epu32(_msg1, _msg2); // SIGMA0 for RUN 52
+
+            // round 48 - 51
+            _msg0 = _mm_sha256msg2_epu32(_msg0, _msg3);
+            msg = _mm_add_epi32(_msg0, _mm_set_epi32(0x34b0bcb5, 0x2748774c, 0x1e376c08, 0x19a4c116));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg0, _msg3, 4); // W[t-7] for RUN 36
+            _msg1 = _mm_add_epi32(_msg1, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg2 = _mm_sha256msg1_epu32(_msg2, _msg3); // SIGMA0 for RUN 40
+
+            // round 52 - 55
+            _msg1 = _mm_sha256msg2_epu32(_msg1, _msg0);
+            msg = _mm_add_epi32(_msg1, _mm_set_epi32(0x682e6ff3, 0x5b9cca4f, 0x4ed8aa4a, 0x391c0cb3));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg1, _msg0, 4); // W[t-7] for RUN 40
+            _msg2 = _mm_add_epi32(_msg2, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg3 = _mm_sha256msg1_epu32(_msg3, _msg0); // SIGMA0 for RUN 44
+
+            // round 55 - 59
+            _msg2 = _mm_sha256msg2_epu32(_msg2, _msg1);
+            msg = _mm_add_epi32(_msg2, _mm_set_epi32(0x8cc70208, 0x84c87814, 0x78a5636f, 0x748f82ee));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg2, _msg1, 4); // W[t-7] for RUN 44
+            _msg3 = _mm_add_epi32(_msg3, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg0 = _mm_sha256msg1_epu32(_msg0, _msg1); // SIGMA0 for RUN 48
+
+            // round 60 - 63
+            _msg3 = _mm_sha256msg2_epu32(_msg3, _msg2);
+            msg = _mm_add_epi32(_msg3, _mm_set_epi32(0xc67178f2, 0xbef9a3f7, 0xa4506ceb, 0x90befffa));
+            CDGH = _mm_sha256rnds2_epu32(CDGH, ABEF, msg);
+            DEBUG_ABCDEFGH(CDGH, ABEF);
+            msg = _mm_shuffle_epi32(msg, 0b00001110); // flip 64 bytes
+            ABEF = _mm_sha256rnds2_epu32(ABEF, CDGH, msg);
+            DEBUG_ABCDEFGH(ABEF, CDGH);
+
+            t_0 = _mm_alignr_epi8(_msg3, _msg2, 4); // W[t-7] for RUN 48
+            _msg0 = _mm_add_epi32(_msg0, t_0); // W[t-16] + sigma0 + W[t-7] 
+            _msg1 = _mm_sha256msg1_epu32(_msg1, _msg2); // SIGMA0 for RUN 52
+
+            hash_ABEF = _mm_add_epi32(hash_ABEF, ABEF);
+            hash_CDGH = _mm_add_epi32(hash_CDGH, CDGH);
+            DEBUG_ABCDEFGH(hash_ABEF, hash_CDGH);
         }
+
+        // A B E F
+        // 3 2 1 0
+        // C D G H
+        //  
+        hash_CDGH = _mm_shuffle_epi32(hash_CDGH, 0b00'01'10'11); // E F A B
+        __m128i ABCD = _mm_alignr_epi8(hash_CDGH, hash_ABEF, 8);
+        ABCD = _mm_shuffle_epi8(ABCD, big_endian_flip_mask);
+        
+#ifdef DEBUG
+        std::cout << std::hex << std::setfill('0') << "ABCD = ";
+        word ABCD_words[4];
+        _mm_storeu_si128((__m128i_u*) ABCD_words, ABCD);
+        for (int i = 0; i < 4; ++i) std::cout << ABCD_words[i] << " ";
+        std::cout << std::dec << std::setfill(' ') << "\n";
+#endif
 
         delete[] blks;
         return {};
